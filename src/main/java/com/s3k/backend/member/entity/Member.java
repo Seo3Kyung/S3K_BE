9c9765fc0d71af6ca48915c4b65ac21647fe89d0
hyensukim <crazymin17@naver.com>
2025-05-19 00:17:55 +0900

    [feature-001] 인증/인가 관련 공통 사항 구현

---
+++ src/main/java/com/s3k/backend/member/entity/Member.java
@@ -0,0 +1,26 @@
+package com.s3k.backend.member.entity;
+
+import com.s3k.backend.member.enums.Role;
+import com.s3k.backend.member.enums.Sns;
+import java.time.LocalDateTime;
+import jdk.jshell.Snippet.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Member {
+  private Long id; // 서비스 회원 ID
+  private String snsId; // SNS 고유 식별 ID
+  private Sns sns; // SNS 종류
+  private String nickname; // 서비스 회원 닉네임
+//  private String memberId; - snsId로 대체
+//  private String password; - snsId로 대체
+  private boolean tos; // 이용 약관
+  private boolean privacyPolicy; // 개인정보 수집 및 이용 동의
+  private LocalDateTime agreeAt; // 개인정보 수집 및 이용 동의일자
+  private Role role; // 서비스 회원 권한
+  private Status status; // 서비스 회원 상태
+  private LocalDateTime createdAt; // 서비스 회원 정보 생성 일자(가입)
+  private LocalDateTime updatedAt; // 서비스 회원 정보 수정 일자
+}
---
+++ src/main/java/com/s3k/backend/member/enums/Sns.java
@@ -0,0 +1,15 @@
+package com.s3k.backend.member.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Sns {
+  UNKNOWN(0,"미정의"),
+  KAKAO(1,"카카오"),
+  NAVER(2,"네이버"),
+  GOOGLE(3,"구글");
+  private final int value;
+  private final String desc;
+}
