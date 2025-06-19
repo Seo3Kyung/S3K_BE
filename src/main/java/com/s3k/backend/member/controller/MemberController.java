f561d1b58a35ac41cca5f0835409ea595dd8bd1b
Tedeeeee <gksrmf359@gmail.com>
2025-06-19 23:36:36 +0900

    [feature-007] 회원가입 완료 ( 사진 작업 미진행 )

--- src/main/java/com/s3k/backend/member/controller/MemberController.java
+++ src/main/java/com/s3k/backend/member/controller/MemberController.java
@@ -1,7 +1,7 @@
 package com.s3k.backend.member.controller;
 
-
-import com.s3k.backend.member.dto.MemberDefaultDto;
+import com.s3k.backend.global.dto.ApisResponse;
+import com.s3k.backend.global.util.ValidatorUtils;
 import com.s3k.backend.member.dto.MemberSignupDto;
 import com.s3k.backend.member.entity.Member;
 import com.s3k.backend.member.service.MemberService;
@@ -9,10 +9,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,32 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class MemberController {
 
+  private static final String NICKNAME = "nickname";
+
   private final MemberService memberService;
 
-  @GetMapping("/test")
-  public String test() {
-//    log.error("디스코드 에러 테스트");
-    return "에러";
+  @GetMapping("/nickname/available")
+  public ApisResponse<Boolean> checkNickname(@RequestParam("nickname") String nickname,
+      @AuthenticationPrincipal Member principal) {
+    ValidatorUtils.validate(NICKNAME, nickname);
+    return ApisResponse.ok(memberService.checkNicknameAvailability(nickname, principal.getSnsId()));
   }
 
-  @GetMapping("/authTest")
-  public String authTest() {
-    return "인증 테스트 성공";
-  }
-
-//  @PostMapping("/sign-up/{sns}")
-//  public MemberDefaultDto.Response signUp(
-//      @PathVariable String sns,
-//      @RequestBody MemberSignupDto.Request request
-//  ) {
-    // TODO : 예외처리 공통화 처리 후 수정.
-//    return memberService.signup(sns.toUpperCase(), request);
-//  }
-
-  @PostMapping("/test/sign-up")
-  public String signUp(@RequestBody MemberSignupDto.Request request,
-      @AuthenticationPrincipal Member member) {
-    String snsId = member.getSnsId();
-    return "회원가입 API는 SNS를 통해서만 가능합니다.";
+  @PostMapping("/signup")
+  public ApisResponse<?> signUp(@RequestBody MemberSignupDto.Request request,
+      @AuthenticationPrincipal Member principal) {
+    memberService.registerMember(request, principal.getSnsId());
+    return ApisResponse.ok();
   }
 }
